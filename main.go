08af78dea493415ced7d22b6b13462f5e4eb85ea
Skyler Burger <skylerb@splunk.com>
2021-08-11 11:54:48 -0700

    Add example of iIIFE

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 )
 
+// Example of a function closure
 func returnGreetingFunc (salutation string) (greetingFunc func (string)) {
 	greetingFunc = func (name string) {
 		fmt.Printf("%v, %v", salutation, name)
@@ -15,26 +16,32 @@ func returnGreetingFunc (salutation string) (greetingFunc func (string)) {
 }
 
 func main() {
-	newFunc := returnGreetingFunc("Good morning")
-	newFunc("Skyler")
+	//newFunc := returnGreetingFunc("Good morning")
+	//newFunc("Skyler")
+
+	// If a function is returned, you can immediately invoke it and pass args
+	returnGreetingFunc("Good morning")("Skyler")
 }
 
+// Example of reading in user input from the CLI
 func intro () {
+	// Create a Scanner instance
 	scanner := bufio.NewScanner(os.Stdin)
 	
-	// Name as String
 	fmt.Print("What is your name?: ")
+	// .Scan() when you want to read in user input
 	scanner.Scan()
+	// .Text() when you want to access the text that was scanned
 	name := scanner.Text()
 	
 	fmt.Print("What is your age?: ")
 	scanner.Scan()
+	// Convert string type to integer type
 	age, _ := strconv.ParseInt(scanner.Text(), 10, 8)
 	fmt.Printf("Your name is %v and your age is %d years old.\n", name, age)
 	
 	year := 2021 - age
 	fmt.Printf("Were you born in the year %v? (y/n): ", year)
-
 	scanner.Scan()
 	response := scanner.Text()
 
